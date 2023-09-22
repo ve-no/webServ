dbfe9a2165d671cc36c595ec5826ae5178a0f594
ve-no <allal.elbakourii@gmail.com>
2023-09-22 11:14:01 +0100

    starting working for configFile

---
+++ inc/ConfigFile.hpp
@@ -0,0 +1,18 @@
+#ifndef CONFIGFILE_HPP
+#define CONFIGFILE_HPP
+
+#include "Webserv.hpp"
+
+class ConfigFile {
+	public:
+		ConfigFile();
+		~ConfigFile();
+
+		static int getTypePath(std::string const path);
+		static int accessFile(std::string const path, int mode);
+		std::string	fileToString(std::string path);
+		static int isReadableAndExist(std::string const path, std::string const index);
+
+};
+
+#endif
